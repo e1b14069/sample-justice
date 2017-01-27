78a1350428ccf2417ba677ba5a5b69dd0039d372
e1b14069 <e1b14069@st.oit.ac.jp>
2017-01-27 18:16:49 +0900

    スクリプトの作成
    script.jsファイルの変更をした

---
+++ keijiban/WebContent/js/script.js
@@ -0,0 +1,17 @@
+function loadMessages(room){
+    Keijiban.getMessages(room, function(reply){
+        var $board = $(".keijiban");
+
+        $board.empty();
+        $board.append("<h3>" + room + "</h3>");
+        for(var i=0; i<reply.length; i++){
+            $board.append("<h4 class='name' style='display:inline'>"+reply[i].name+"</h4>");
+            $board.append(" <h6 class='time' style='display:inline'>"+reply[i].time+"</h6>");
+            $board.append("<p class='message'>"+reply[i].message+"</p>");
+        }
+    });
+}
+
+$(document.ready(function(){
+    loadMessages("test");
+});
